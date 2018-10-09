5da1d49b354db895bdd0a8bb75ae1dc8410c034c
HakonArnarS <43438456+HakonArnarS@users.noreply.github.com>
2018-10-09 18:21:31 +0000

    jsverkefni1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var ssvar1 = "Hello" + " World!";
+
+var nsvar2 = 2+5-3*7/4;
+
+var bsvar3 = true||false && !false; 
+
+var bsvar4 = "Jón"==="jón"; 
+
+var bsvar5 = 7<=7!==4; 
+
+var ssvar6 = "true því að aboutKalli er það sama og Kalli er góður strákur" + " og isItTrueWhatTheySayAboutKalli er það sama og aboutKalli og er því það sama og Kalli er góður strákur."; 
+
+var ssvar7 = "true því aboutPalli er það sama og Palli er vondur strákur" + " og isItTrueWhatTheySayAboutPalli er það sama og aboutPalli OG !palli er góður strákur sem þýðir í raun Palli er ekki góður strákur eða Palli er vondur strákur."; 
+
+var ssvar8a = "!! er í raun not not. Ef !0 er true verður !!0 þá false.";
+var ssvar8b = "!-2 er false og !!-2 verður því öfugt við það eða true.";
+var ssvar8c = "!"" er true og !!"" verður því öfugt við það eða false.";  
+var ssvar8d = "Ef kalli er true til að byrja með, verður !kalli false en !!kalli svo aftur true"; 
+
+var ssvar9 = "true því myNumber er jafnt og 5 og 5 er minna en 9 og 9 er einnig meira en 2"; 
